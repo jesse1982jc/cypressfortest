4113a40b319066efc08e308a5d376887e8c522c9
Jesse <ya710728@gmail.com>
2024-09-11 18:07:58 +0800

    add Tc09 browser control back forward reload

---
+++ cypress/e2e/Tc09_BrowserControl.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe("Advanced UI Elements", () => {
+  it("Browser Controls", () => {
+    cy.visit("https://react-redux.realworld.io");
+    cy.contains("Sign in").click();
+    cy.go("back"); // 回瀏覽器的"上一頁"
+    cy.go("forward"); // 回瀏覽器的"下一頁"
+    cy.go(-1); // 一樣是回瀏覽器的"上一頁"
+    cy.go(1); // 一樣是回瀏覽器的"下一頁"
+    cy.get('input[type="email"]').type("jcjchuhu2@gmail.com");
+    cy.reload();
+  });
+});
