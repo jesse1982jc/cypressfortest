32b1aa5bac46333bfd4d6f9f0451ce06a185364f
Jesse <ya710728@gmail.com>
2024-09-11 13:48:06 +0800

    add tc06 some note

--- cypress/e2e/Tc06_WebTable.spec.js
+++ cypress/e2e/Tc06_WebTable.spec.js
@@ -10,6 +10,7 @@ describe("Advanced UI Elements", () => {
       .should("be.visible");
 
     // 2. Check value present in the specific row and col
+    // 2-1. 找到 table下>tbody下>第三行(橫) tr 的 第三個 cell 的 td 元素
     cy.get("#table1>tbody>tr:nth-child(1)>td:nth-child(3)")
       .contains("td", "jsmith@gmail.com")
       .should("be.visible");
@@ -20,11 +21,18 @@ describe("Advanced UI Elements", () => {
     // );
 
     // 3. Check value based on the condition
+    // 3-1. 找到 table下>tbody下>所有的 tr 橫列 當中的 第二個 td 元素，以範例來說: 共 4 個
+    // 3-2. 然後用 each () 遍歷這個 column 的 td，取名為 ele(元素)、有索引 index、用 list 來當作取出來的列表(沒用到)
     cy.get("#table1>tbody>tr>td:nth-child(2)").each((ele, index, list) => {
+      // 把取出來的元素 ele，抓取出文字來，丟給 eleForText 變數
       const eleForText = ele.text();
 
       // 備註: 這邊的 each() 裡的 list 用不到
-      //   if (eleForText == "Jason") {
+
+      // if (eleForText == "Jason") {
+      // 3-4. 假如 eleForText 有包含 "Jason" 字串的話，就去抓取 同行(看 index ) 的 第四個 cell 的 td 元素
+      // 3-5. 把抓取出來的元素命名為 due，再把 due 抓取出文字出來，命名為 dueAmount
+      // 3-6. 斷言預期 dueAmount 要等於 "$100.00"
       if (eleForText.includes("Jason")) {
         cy.get("#table1>tbody>tr>td:nth-child(4)")
           .eq(index)
---
+++ cypress/e2e/Tc07_Iframes.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe("Advanced UI Elements", () => {
+  it("Iframes", () => {
+    cy.visit("https://the-internet.herokuapp.com/iframe");
+  });
+});
