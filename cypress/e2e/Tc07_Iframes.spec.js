c6dcedfc12b605253f45f45f3a3cf1b263fb80f9
Jesse <ya710728@gmail.com>
2024-09-11 16:30:21 +0800

    add tc07 but still run error

--- cypress/e2e/Tc07_Iframes.spec.js
+++ cypress/e2e/Tc07_Iframes.spec.js
@@ -3,5 +3,17 @@
 describe("Advanced UI Elements", () => {
   it("Iframes", () => {
     cy.visit("https://the-internet.herokuapp.com/iframe");
+
+    // 先清掉小彈窗訊息
+    cy.get(
+      'button[class="tox-notification__dismiss tox-button tox-button--naked tox-button--icon"]'
+    ).click();
+
+    // 以下程式有問題，執行有誤
+    cy.get("#mce_0_ifr").within(($iframe) => {
+      cy.wait(1500);
+      const element = $iframe.contents().find("#tinymce");
+      cy.wrap(element).clear().type("Hello TextBox");
+    });
   });
 });
